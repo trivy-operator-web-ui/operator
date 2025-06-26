0dbfdfd38d3b539c630d7be84ca75f246db513f0
ABWassim <wassim.belkacem99@gmail.com>
2025-06-26 15:21:38 +0200

    feat: shared data between operator and API

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use futures::prelude::*;
 use kube::{
-    api::Api,
+    api::{Api, ObjectMeta},
     runtime::{watcher, WatchStreamExt},
     Client,
 };
 
-use std::result::Result::Ok;
+use web::{Data};
+
+use std::{result::Result::Ok, sync::{Arc, Mutex}};
 
 use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 
@@ -13,15 +15,32 @@ use tokio;
 
 mod data;
 use data::{VulnerabilityReportSpec};
-use tracing::info;
 
-
-#[get("/vulnreports")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+#[derive(Clone, Default)]
+struct State {
+    vulnerability_reports: Arc<Mutex<Vec<VulnerabilityReportSpec>>>, // Mutex is necessary to mutate safely across threads
 }
 
-async fn start_controller() -> anyhow::Result<()> {
+#[get("/vulnreports")]
+async fn hello(data: Data<State>) -> impl Responder {
+    let vulnerability_reports = data.vulnerability_reports.lock().unwrap().clone();
+
+    HttpResponse::Ok().json(&vulnerability_reports)
+}
+
+fn simplify_metadata(metadata: &mut ObjectMeta) {
+    metadata.finalizers = None;
+    metadata.managed_fields = None;
+    metadata.generation = None;
+    metadata.creation_timestamp = None;
+    metadata.annotations = None;
+    metadata.owner_references = None;
+    metadata.resource_version = None;
+    metadata.uid = None;
+    metadata.labels = None;
+}
+
+async fn start_controller(s: State) -> anyhow::Result<()> {
     let client = Client::try_default().await?;
 
     let api: Api<VulnerabilityReportSpec> = Api::all(client);
@@ -29,22 +48,24 @@ async fn start_controller() -> anyhow::Result<()> {
 
     let mut stream = watcher(api, wc).applied_objects().boxed();
 
-    while let Some(event) = &stream.try_next().await? {
-        println!(
-            "Applied: {}",
-            event.report.artifact.repository.clone().unwrap_or_default()
-        );
+    while let Some(mut event) = stream.try_next().await? {
+        simplify_metadata(&mut event.metadata);
+    
+        s.vulnerability_reports.lock().unwrap().push(event);
     }
     Ok(())
 }
 
-
 #[tokio::main]
-async fn main() -> anyhow::Result<()> {    
-    let controller = start_controller();
+async fn main() -> anyhow::Result<()> {
 
-    let server = HttpServer::new ( || {
+    let state = State::default();
+
+    let controller = start_controller(state.clone());
+
+    let server = HttpServer::new ( move || {
     App::new()
+    .app_data(Data::new(state.clone()))
         .service(web::scope("/api").service(hello))
     })
     .bind(("0.0.0.0", 8080))?
