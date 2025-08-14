585bc9463b953020e9126492c13e79ccc6e14962
ABWassim <wassim.belkacem99@gmail.com>
2025-08-14 14:43:16 +0200

    fix: send image ref instead of report name

--- src/dto/mod.rs
+++ src/dto/mod.rs
@@ -1,4 +1,4 @@
-use crate::kubedata::{Artifact, Summary, VulnerabilityReport};
+use crate::kubedata::{Summary, VulnerabilityReport};
 
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
@@ -6,26 +6,30 @@ use serde::{Deserialize, Serialize};
 /// DTO returned to the frontend
 #[derive(Serialize, Deserialize, Clone, Debug, JsonSchema)]
 pub struct SimpleVulnerabilityReport {
+    /// Uid of the Vulnerability Report resource
+    pub uid: String,
+
+    /// Image in the format <repository>:<tag>
+    pub image: String,
+
+    /// Namespace of the vulnerability report
+    pub namespace: String,
+
     /// Summary is a summary of Vulnerability counts grouped by Severity.
     pub summary: Summary,
-
-    /// Name of the Vulnerability Report
-    pub name: String,
-
-    /// Artifact represents a standalone, executable package of software that includes everything needed to
-    /// run an application.
-    pub artifact: Artifact,
-
-    pub uid: String,
 }
 
 impl From<VulnerabilityReport> for SimpleVulnerabilityReport {
     fn from(v: VulnerabilityReport) -> Self {
         Self {
-            name: v.metadata.name.unwrap_or_default(),
-            summary: v.report.summary,
-            artifact: v.report.artifact,
             uid: v.metadata.uid.unwrap_or_default(),
+            image: format!(
+                "{}:{}",
+                v.report.artifact.repository.unwrap_or_default(),
+                v.report.artifact.tag.unwrap_or_default()
+            ),
+            namespace: v.metadata.namespace.unwrap_or_default(),
+            summary: v.report.summary,
         }
     }
 }
